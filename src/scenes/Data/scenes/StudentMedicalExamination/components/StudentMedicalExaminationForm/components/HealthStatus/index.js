53fdcfff7b9a397132f8970720fceb5823fb24f3
SwiftEsther <swiftestherduro@gmail.com>
2018-03-25 09:15:14 +0100

    merge conflicts fix

--- src/scenes/Data/scenes/StudentMedicalExamination/components/StudentMedicalExaminationForm/components/HealthStatus/index.js
+++ src/scenes/Data/scenes/StudentMedicalExamination/components/StudentMedicalExaminationForm/components/HealthStatus/index.js
@@ -18,24 +18,15 @@ class HealthStatus extends Component {
     if (this.state.value === 'yes') {
       ifYes = (
         <div className="form-label-group">
-<<<<<<< HEAD
           <label htmlFor="reason">
-=======
-          <label htmlFor="surname">
->>>>>>> ea0a49ff517583c8960813b147024eb4a90de934
           Please state the reason for the admission,
           name of the hospital and dates.
           </label>
           <Field
             disabled={this.props.disabled}
             className="form-control"
-<<<<<<< HEAD
             name="reason"
             id="reason"
-=======
-            name="surname"
-            id="surname"
->>>>>>> ea0a49ff517583c8960813b147024eb4a90de934
             component="textarea"
           />
         </div>
